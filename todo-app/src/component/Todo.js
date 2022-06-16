9653358729255952a6a6572f731c430d0b92ea6a
Anuj Jha <anujjha@Anujs-MacBook-Air.local>
2022-06-16 23:47:50 +0530

    To-DoApp

---
+++ todo-app/src/component/Todo.js
@@ -0,0 +1,11 @@
+import {component} from "react";
+
+class Todo extends component{
+    render(){
+        return(
+            <div>This is Todo Component</div>
+        )
+    }
+}
+
+export default Todo;

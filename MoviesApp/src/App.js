a2b0959fe42e11f95f933a30bd4fc28b4793e180
Anuj Jha <anujjha@Anujs-MacBook-Air.local>
2022-06-18 23:49:50 +0530

    added banner component and link for bootstrap

---
+++ MoviesApp/src/App.js
@@ -0,0 +1,16 @@
+import NavBar from './component/NavBar';
+import Banner from './component/Banner';
+import './App.css';
+
+function App() {
+  return (
+
+    <div>
+         <NavBar/>
+         <Banner/>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ MoviesApp/src/component/Banner.js
@@ -0,0 +1,15 @@
+import {Component} from "react"
+
+
+class Banner extends Component{
+    render() {
+        return (
+            <div className="card">
+                <img src="https://images.squarespace-cdn.com/content/v1/5ccabcf60b77bdbb3acaf70a/1567683272122-6PF2QDD6NF68VHMGQJOC/dd5r35v-e8431f3b-2f7a-4b39-9945-fa6611506b83.jpg?format=1000w" className="card-img-top" alt="..."/>
+                    
+            </div>
+        )
+    }
+}
+
+export default Banner

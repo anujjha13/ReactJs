5e538445753cf69cefa5d4b61fa60f02862a21b9
Anuj Jha <anujjha@Anujs-MacBook-Air.local>
2022-06-24 21:26:22 +0530

    added functional of next and previous

--- MoviesApp/src/component/MovieList.js
+++ MoviesApp/src/component/MovieList.js
@@ -1,46 +1,76 @@
 import { Component } from "react";
-import {movies} from "../movieData"
+// import {movies} from "../movieData";
+import axios from 'axios';
 
 class MovieList extends Component{
 
     constructor(){
         super();
         this.state={
-            hover:""
+            hover:"",
+            pArr:[1],
+            movies:[],
+            currPage:1
         };
     }
+
+    async componentDidMount(){
+        console.log("Component Did Mount");
+        const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=11b4db041bf33bf9932e58398b21c951&language=en-US&page=1')
+        console.log(res.data);
+        this.setState({
+            movies:[...res.data.results]
+        })
+    }
+
+
+    changeMovies = async()=>{
+        const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=81242a2aa2066e052c78ec9ac1700c59&language=en-US&page=${this.state.currPage}`)
+        console.log(res.data);
+        this.setState({
+            movies:[...res.data.results]
+        })
+    }
+
+    
+    handleNext=()=>{
+        this.setState({
+            pArr:[...this.state.pArr,this.state.pArr.length+1],
+            currPage:this.state.currPage+1
+        },this.changeMovies)
+    }
+
     render(){
-        let moviesArr = movies.results
+        // let moviesArr = movies.results
         return(
             <>
             <div>
                 <h3 className="text-center"><strong>Trending</strong></h3>
             </div>
             <div className="movies-list">
-                {moviesArr.map((movieEle)=>(
-                   <div className="card movie-card" onMouseEnter={()=>this.setState({hover:movieEle.id})} onMouseLeave={()=>this.setState({hover:""})}>
-                    <img src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} style={{height:'40vh',width:'20vw'}} className="card-img-top movie-img" alt="..."/>
-                    <h5 className="card-title movie-title">{movieEle.title}</h5>
-                    <div style={{display:'flex',justifyContent:"center"}}>
-                        {this.state.hover == movieEle.id && (
+                    {this.state.movies.map((movieEle) => (
+                        <div className="card movie-card" onMouseEnter={()=>this.setState({hover:movieEle.id})} onMouseLeave={()=>this.setState({hover:""})} >
+                            <img src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
+                            <h5 className="card-title movie-title">{movieEle.title}</h5>
+                            <div style={{ display: 'flex', justifyContent: "center" }}>
+                                {this.state.hover == movieEle.id && (
                                 <a href="#" type="button" className="btn btn-primary movies-button">Add to Favourites</a>)}
-                    </div>
-                    
-                     </div>
-                ))}
-            </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             <div style={{display:"flex",justifyContent:"center"}}>
-                    <nav aria-label="Page navigation example">
-                    <ul class="pagination">
-                      <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                        {this.state.pArr.map((ele)=>(
+                            <li className="page-item"><a className="page-link" href="#">{ele}</a></li>
+                        ))}
+                        
+                        <li className="page-item"><button className="page-link" onClick={this.handleNext}>Next</button></li>
                     </ul>
-                     </nav>
-            </div>
-                  
+                </nav>
+                </div>
             </>
         )
     }

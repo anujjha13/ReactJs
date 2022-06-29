0948e7cbdcaf8728c769fc7232417fe84dd3037c
Anuj Jha <anujjha@Anujs-MacBook-Air.local>
2022-06-29 20:57:08 +0530

    browserroute,route,routes,Link used

--- MoviesApp/src/App.js
+++ MoviesApp/src/App.js
@@ -3,15 +3,26 @@ import Banner from './component/Banner';
 import MovieList from  './component/MovieList'
 import Fav from './component/Fav'
 import './App.css';
+import {BrowserRouter,Routes,Route, Router} from 'react-router-dom'
 
 function App() {
   return (
 
     <div>
-         <NavBar/>
+
+    <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<><Banner/><MovieList/></>}/>
+          <Route path="/favourites" element={<Fav/>}/>
+        </Routes>
+    </BrowserRouter>
+
+    
+        {/*<NavBar/>
          {/* <Banner/>
-         <MovieList/> */}
-         <Fav/>
+         <MovieList/> 
+         <Fav/>*/}
     </div>
    
   );
--- MoviesApp/src/component/Fav.js
+++ MoviesApp/src/component/Fav.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { movies } from "../movieData";
 
+
 class Fav extends Component {
 
     

af84eb1a036826df76ceeeab38fbc6fbe2bf8e14
Anuj Jha <anujjha@Anujs-MacBook-Air.local>
2022-07-04 04:37:53 +0530

    added delete functionality

--- MoviesApp/src/component/Fav.js
+++ MoviesApp/src/component/Fav.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { movies } from "../movieData";
 
 
 class Fav extends Component {
@@ -129,6 +130,18 @@ class Fav extends Component {
             movies2:[...temp],
         })
     }
+    handleDelete = (movieEle) =>{
+        let filteredArr = this.state.movies.filter((movieObj)=>{
+            if(movieObj!=movieEle){
+                return true;
+            }
+        })
+
+        this.setState({
+            movies:[...filteredArr],
+            movies2:[...filteredArr]
+        })
+    }
     render() {
         let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western"}
         return (
@@ -157,12 +170,12 @@ class Fav extends Component {
                                 <tr>
                                     <th scope="col">Title</th>
                                     <th scope="col">Genre</th>
-                                    <th scope="col" >
+                                    <th scope="col" style={{marginLeft:"-1rem",display:"flex",justifyContent: "space-between",marginRight: "0.3rem",alignItems:"center"}}>
                                         <i class="fa fa-sort-up" onClick={this.sortpopulairtydesc}></i>
                                         Popularity
                                         <i class="fa fa-sort-down"  onClick={this.sortpopulairtyinc}></i>
                                     </th>
-                                    <th scope="col">
+                                    <th scope="col" style={{marginLeft:"-4rem"}}>
                                         <i class="fa fa-sort-up" onClick={this.sortRatingdesc}></i> 
                                         Rating
                                         <i class="fa fa-sort-down"  onClick={this.sortRatinginc}></i>
@@ -176,9 +189,9 @@ class Fav extends Component {
                                         <tr>
                                             <th scope="row"><img style={{ width: "8rem", padding: "1rem" }} src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} />{movieEle.title}</th>
                                             <td>{genreIds[movieEle.genre_ids[0]]}</td>
-                                            <td>{movieEle.popularity}</td>
+                                            <td >{movieEle.popularity}</td>
                                             <td>{movieEle.vote_average}</td>
-                                            <td><button type="button" className="btn btn-danger">Delete</button></td>
+                                            <td><button type="button" className="btn btn-danger" onClick={()=>this.handleDelete(movieEle)}>Delete</button></td>
                                         </tr>
                                     ))
                                 }

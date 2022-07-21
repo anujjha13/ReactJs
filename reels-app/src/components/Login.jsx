ae8ed892ce7a2a2c6eaa5d5b6f8f802238692e08
Anuj Jha <anujjha@Anujs-MacBook-Air.local>
2022-07-21 23:56:46 +0530

    login functionality added to check on mount user loggedin or not

--- reels-app/src/components/Login.jsx
+++ reels-app/src/components/Login.jsx
@@ -40,68 +40,65 @@ function Login(){
         setUser(null);
     }
 
+    //now we need of useffect because we need everytime we land on login first it check wheather user is logged in previously for that we user onauthstatechanged function
     useEffect(()=>{
         onAuthStateChanged(auth, (user) => {
             if (user) {
-              // User is signed in, see docs for a list of available properties
-              // https://firebase.google.com/docs/reference/js/firebase.User
-            //   const uid = user.uid;
               setUser(user);
-              // ...
+             
             } else {
-              // User is signed out
-              // ...
               setUser(null);
             }
             setMainLoader(false);
         })
-    },[])
+    },[]) //why to pass [] because we want it to work only on mount 
     return (
         <>
+            {/* we are require to use mainloader bcz while checking on mount it will take time and during time it is howing form as well after verifying it removes form and show logged in */}
             {mainLoader == true?<h1>Page is Loading..</h1>:
             error != "" ? <h1> {error}</h1>:
              loader == true?<h1>...Loading</h1>:
                 user != null ? <><h1>User is {user.uid}</h1> <button type="click" onClick={logOut}>Logout</button> </>: 
                 <>
 
-<main class="flex align-items-center justify-content-center">
-        <section id="mobile" class="flex">
-        </section>
-        <section id="auth" class="flex direction-column">
-            <div class="panel login flex direction-column">
-                <h1 title="Instagram" class="flex justify-content-center">
-                    <img src="./logo.svg" alt="Instagram logo" title="Instagram logo" />
-                </h1>
-                <h2 class="flex justify-content-center">#INSTAREELS</h2>
-                <form>
-                    <label for="email" class="sr-only">Telefone, nome de usuário ou e-mail</label>
-                    <input type="email" onChange={trackemail} placeholder="Enter email"/>
+                <main class="flex align-items-center justify-content-center">
+                        <section id="mobile" class="flex">
+                        </section>
+                        <section id="auth" class="flex direction-column">
+                            <div class="panel login flex direction-column">
+                                <h1 title="Instagram" class="flex justify-content-center">
+                                    <img src="./logo.svg" alt="Instagram logo" title="Instagram logo" />
+                                </h1>
+                                <h2 class="flex justify-content-center">#INSTAREELS</h2>
+                                <form>
+                                    <label for="email" class="sr-only">Telefone, nome de usuário ou e-mail</label>
+                                    <input type="email" onChange={trackemail} placeholder="Enter email"/>
 
-                    <label for="password" class="sr-only">Senha</label>
-                    <input type="password" onChange={trackpassword} placeholder="Enter password"/>
+                                    <label for="password" class="sr-only">Senha</label>
+                                    <input type="password" onChange={trackpassword} placeholder="Enter password"/>
 
-                     <button type="click" onClick={printdetails}>Login</button> 
-                </form>
-                <div class="flex separator align-items-center">
-                    <span></span>
-                    <div class="or">OU</div>
-                    <span></span>
-                </div>
-                <div class="login-with-fb flex direction-column align-items-center">
-                    <div>
-                        <img />
-                        <a>Login using Google</a>
-                    </div>
-                    <a href="#">Forgot Your Password?</a>
-                </div>
-            </div>
-            <div class="panel register flex justify-content-center">
-                <p>Don't you have an account?</p>
-                <a href="#">Sign-Up</a>
-            </div>
-            
-        </section>
-    </main>
+                                    <button type="click" onClick={printdetails}>Login</button> 
+                                </form>
+                                <div class="flex separator align-items-center">
+                                    <span></span>
+                                    <div class="or">OU</div>
+                                    <span></span>
+                                </div>
+                                <div class="login-with-fb flex direction-column align-items-center">
+                                    <div>
+                                        <img />
+                                        <a>Login using Google</a>
+                                    </div>
+                                    <a href="#">Forgot Your Password?</a>
+                                </div>
+                            </div>
+                            <div class="panel register flex justify-content-center">
+                                <p>Don't you have an account?</p>
+                                <a href="#">Sign-Up</a>
+                            </div>
+                            
+                        </section>
+                    </main>
      </> }
         </>
     )
